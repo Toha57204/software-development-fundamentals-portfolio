71d58df713b8722c2bc4eec2e722cba4f8bbcc9d
Toha <sfttohasharker123@gmail.com>
2026-08-05 12:52:24 +1000

    Add first Python program

---
+++ lesson-01-programming-language-basics/first_program.py
@@ -0,0 +1,13 @@
+"""Display a welcome message for ITAP1001."""
+
+# My first Python program for Software Development Fundamentals
+
+
+def main():
+    """Run the program."""
+    print("Welcome to ITAP1001 Software Development Fundamentals")
+    print("I am beginning my Python programming journey.")
+
+
+if __name__ == "__main__":
+    main()
